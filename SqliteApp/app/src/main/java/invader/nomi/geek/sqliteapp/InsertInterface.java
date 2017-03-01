9ce443f9eace47808f325749af9e586a825ed656
Alihaider786 <alisheikh405@gmail.com>
2017-03-01 13:43:09 -0800

    New project added

---
+++ SqliteApp/app/src/main/java/invader/nomi/geek/sqliteapp/InsertInterface.java
@@ -0,0 +1,8 @@
+package invader.nomi.geek.sqliteapp;
+
+/**
+ * Created by GEO on 2/18/2017.
+ */
+public interface InsertInterface {
+    void getResponse(boolean result);
+}
---
+++ SqliteApp/app/src/main/java/invader/nomi/geek/sqliteapp/UpdateInterface.java
@@ -0,0 +1,9 @@
+package invader.nomi.geek.sqliteapp;
+
+/**
+ * Created by ALi on 2/21/2017.
+ */
+
+public interface UpdateInterface {
+   void onUpdateResponce(long responce);
+}
