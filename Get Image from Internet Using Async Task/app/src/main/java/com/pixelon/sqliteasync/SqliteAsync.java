bb066d6f51f765befaa5f5523b87d40a7a668109
Alihaider786 <alisheikh405@gmail.com>
2017-05-20 07:03:00 -0700

    get image using async task

---
+++ Get Image from Internet Using Async Task/app/src/main/java/com/pixelon/sqliteasync/SqliteAsync.java
@@ -0,0 +1,16 @@
+package com.pixelon.sqliteasync;
+
+import android.app.Application;
+
+/**
+ * Created by ALi on 5/20/2017.
+ */
+
+public class SqliteAsync extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
